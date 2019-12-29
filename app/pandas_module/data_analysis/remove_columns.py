089080f13a827c4afdfe8834542fc63ea1450880
Sovan Panda <Sovan@factspan.blr>
2019-12-29 13:16:03 +0530

    remove_columns logic added

--- app/pandas_module/data_analysis/remove_columns.py
+++ app/pandas_module/data_analysis/remove_columns.py
@@ -4,5 +4,17 @@ import pandas as pd
 How do I remove columns from pandas dataframe?
 """
 
+ufo = pd.read_csv('http://bit.ly/uforeports')
+print(ufo.head())
 
+#remove single column 'Colors Reported'
+ufo.drop('Colors Reported', axis=1, inplace=True)
+print(ufo.head())
 
+#remove more columns in dataframe
+ufo.drop(['City', 'Shape Reported'], axis=1, inplace=True)
+print(ufo.head())
+
+# remove first two rows
+ufo.drop([0,1,2], inplace=True)
+print(ufo.head())
